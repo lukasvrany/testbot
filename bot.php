29b1770a79732622e53fd82ba58178c78efef73d
Lukáš Vraný <lukas.vrany@bonami.cz>
2018-04-07 13:47:06 +0200

    Bot example

---
+++ bot.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+require 'vendor/autoload.php';
+use PhpSlackBot\Bot;
+
+// Custom command
+class MyCommand extends \PhpSlackBot\Command\BaseCommand {
+
+	protected function configure() {
+		$this->setName('mycommand');
+	}
+
+	protected function execute($message, $context) {
+		$this->send($this->getCurrentChannel(), null, 'Hello !');
+	}
+
+}
+
+$bot = new Bot();
+$bot->setToken('SLACK_TOKEN'); // Get your token here https://my.slack.com/services/new/bot
+$bot->loadCommand(new MyCommand());
+$bot->loadInternalCommands(); // This loads example commands
+$bot->run();
