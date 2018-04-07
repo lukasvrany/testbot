2881670d759411b4b81b94e0b1c0f33187748268
Lukáš Vraný <lukas.vrany@bonami.cz>
2018-04-07 15:13:14 +0200

    fixup! Bot example

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ class MyCommand extends \PhpSlackBot\Command\BaseCommand {
 }
 
 $bot = new Bot();
-$bot->setToken('xoxb-335005620384-xWxG9BeFN9nRpLc6ELnSUaPt'); // Get your token here https://my.slack.com/services/new/bot
+$bot->setToken('SLACK'); // Get your token here https://my.slack.com/services/new/bot
 $bot->loadCommand(new MyCommand());
 $bot->loadInternalCommands(); // This loads example commands
 $bot->run();
