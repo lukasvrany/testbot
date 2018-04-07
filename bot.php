46902534126a0db8f083e51c387972ade278a8c1
Lukáš Vraný <lukas.vrany@bonami.cz>
2018-04-07 16:06:48 +0200

    Add new command

--- bot.php
+++ bot.php
@@ -10,8 +10,13 @@ class MyCommand extends \PhpSlackBot\Command\BaseCommand {
 		$this->setName('mycommand');
 	}
 
-	protected function execute($message, $context) {
-		$this->send($this->getCurrentChannel(), null, 'Hello !');
+	protected function execute($data, $context) {
+		if ($data['type'] == 'reaction_added') {
+			$channel = $this->getChannelNameFromChannelId($data['channel']);
+			$username = $this->getUserNameFromUserId($data['user']);
+			echo $username.' from '.($channel ? $channel : 'DIRECT MESSAGE').' : '.$data['text'].PHP_EOL;
+			$this->send($this->getCurrentChannel(), $this->getCurrentUser(), $data['reaction']);
+		}
 	}
 
 }
